0e3688fab56f989135ca10ac56d02c775b87b28c
Robert Lyn <lenny4422@gmail.com>
2018-09-12 00:34:17 -0400

    Added QuickSort

---
+++ CollectionsWithoutWings.Tests/Sorting/QuickSortExtensionTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xunit;
+using System.Collections.Generic;
+using CollectionsWithoutWings.Sorting;
+
+
+namespace CollectionsWithoutWiings.Tests.Sorting
+{
+    public class QuickSortExtensionTests
+    {
+        [Fact]
+        public void QuickSortSort_Null_Exception()
+        {
+            var list = new int[] { };
+
+            Assert.Throws<ArgumentNullException>(() => list.QuickSort(null));
+        }
+
+        [Fact]
+        public void QuickSort_List_SortedList()
+        {
+            foreach (var lst in Data.TestLists.Lists)
+            {
+                var expectedList = new List<int>(lst);
+                expectedList.Sort();
+
+                var testList = new List<int>(lst);
+                testList.QuickSort();
+
+                Assert.Equal(expectedList, testList);
+            }
+        }
+
+        [Fact]
+        public void QuickSort_ListComparer_SortedList()
+        {
+            foreach (var lst in Data.TestLists.Lists)
+            {
+                var expectedList = new List<int>(lst);
+                expectedList.Sort();
+
+                var testList = new List<int>(lst);
+                testList.QuickSort(new Data.IntComparer());
+
+                Assert.Equal(expectedList, testList);
+            }
+        }
+    }
+}
---
+++ CollectionsWithoutWings/Sorting/QuickSortExtension.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionsWithoutWings.Sorting
+{
+    public static class QuickSortExtension
+    {
+        public static void QuickSort<T>(this IList<T> list) where T : IComparable<T>
+        {
+            QuickSortHelper(list, null);
+        }
+
+        public static void QuickSort<T>(this IList<T> list, IComparer<T> comparer)
+        {
+            if (comparer == null)
+                throw new ArgumentNullException(nameof(comparer));
+
+            QuickSortHelper(list, comparer);
+        }
+
+        private static void QuickSortHelper<T>(this IList<T> list, IComparer<T> comparer)
+        {
+            Func<T, T, int> compareFunction;
+
+            if (comparer == null)
+            {
+                compareFunction = (x, y) => ((IComparable<T>)x).CompareTo(y);
+            }
+            else
+            {
+                compareFunction = (x, y) => comparer.Compare(x, y);
+            }
+
+            QuickSortHelper(list, 0, list.Count - 1, compareFunction);
+        }
+
+        private static void QuickSortHelper<T>(IList<T> list, int low, int high, Func<T, T, int> compareFunction)
+        {
+            if (low < high)
+            {
+                var p = PartitionHelper(list, low, high, compareFunction);
+                QuickSortHelper(list, low, p, compareFunction);
+                QuickSortHelper(list, p + 1, high, compareFunction);
+            }
+        }
+
+        private static int PartitionHelper<T>(IList<T> list, int low, int high, Func<T, T, int> compareFunction)
+        {
+            var pivot = list[low];
+            var i = low - 1;
+            var j = high + 1;
+
+            while (true)
+            {
+                do
+                {
+                    i = i + 1;
+                } while (compareFunction(list[i], pivot) < 0);
+
+                do
+                {
+                    j = j - 1;
+                } while (compareFunction(list[j], pivot) > 0);
+
+                if (i >= j)
+                {
+                    return j;
+                }
+
+                Swap(list, i, j);
+            }
+        }
+
+        private static void Swap<T>(IList<T> list, int v, int i)
+        {
+            var tmp = list[v];
+            list[v] = list[i];
+            list[i] = tmp;
+        }
+    }
+}
